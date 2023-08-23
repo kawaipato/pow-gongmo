c787f8d3bd1d77c3fef70d78f93467b345b2ff4d
kawaipato <kis00123@naver.com>
2023-08-23 16:52:56 +0900

    wow

--- code/데이터분석/요인분석.R
+++ code/데이터분석/요인분석.R
@@ -1,6 +1,6 @@
-df = read.csv('/Users/seoihwan/Desktop/escuela/문화관광빅데이터/문화관광스케일링2.csv',encoding='utf-kr')
+df = read.csv('/Users/seoihwan/Desktop/escuela/문화관광빅데이터/pow-gongmo/data/final/final_animal_visit_travel_검색건수_eng.csv')
 head(df)
-summary(df)
+df = read.csv('/Users/seoihwan/Desktop/escuela/문화관광빅데이터/문스4.csv')
 # 결측치 확인
 colSums(is.na(df))
 df = df[,-9]
@@ -15,9 +15,11 @@ eigen(cor(df))
 # 요인 2,3개 만들기
 pc2 = principal(df, nfactors=2, rotate='varimax')
 pc3 = principal(df, nfactors=3, rotate='varimax')
-print.psych(pc3, cut = .3, sort=T)
+pc4 = principal(df, nfactors=4, rotate='varimax')
+print.psych(pc3, sort=T)
+print.psych(pc4, sort=T)
 print.psych(pc2, cut = .3, sort=T)
-
+summary(pc3)
 # 요인분석 적절한지 적합성검정
 install.packages('REdaS')
 library('REdaS')
@@ -29,7 +31,9 @@ install.packages('semPlot')
 library('semPlot')
 semPaths(pc2, what='est',residuals=F,cut=.3,posCol=c('white','darkgreen'),
          negCol=c('white','red'),edge.label.cex=.75)
-semPaths(pc3, what='est',residuals=F,cut=.6,posCol=c('white','darkgreen'),
+semPaths(pc3, what='est',residuals=F,cut=.5,posCol=c('white','darkgreen'),
+         negCol=c('white','red'),edge.label.cex=.75)
+semPaths(pc4, what='est',residuals=F,cut=.5,posCol=c('white','darkgreen'),
          negCol=c('white','red'),edge.label.cex=.75)
 write.csv(pc3$scores,'FA.csv',row.names = F)
 
